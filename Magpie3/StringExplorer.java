0f164122aff45336500f6396e22fd15200a9a112
Jorge Monzon Diaz <jorgemd503@gmail.com>
2019-07-07 16:20:02 -0400

    Add files via upload

---
+++ Magpie3/StringExplorer.java
@@ -0,0 +1,32 @@
+/**
+ * A program to allow students to try out different 
+ * String methods. 
+ * @author Laurie White
+ * @version April 2012
+ */
+public class StringExplorer
+{
+
+    public static void main(String[] args)
+    {
+        String sample = "The quick brown fox jumped over the lazy dog.";
+        
+        //  Demonstrate the indexOf method.
+        int position = sample.indexOf("quick");
+        System.out.println ("sample.indexOf(\"quick\") = " + position);
+        
+        //  Demonstrate the toLowerCase method.
+        String lowerCase = sample.toLowerCase();
+        System.out.println ("sample.toLowerCase() = " + lowerCase);
+        System.out.println ("After toLowerCase(), sample = " + sample);
+        
+        //  Try other methods here:
+        //What am I supposed to do?
+        //Where am I supposed to do it?
+        //When will I do it?
+        //Who will I see when I do it?
+        //All the answers and more on The Asher Show.
+        
+    }
+
+}
